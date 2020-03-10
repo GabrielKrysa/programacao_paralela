687b8c4c46033ae6ea12a50829d340ad76b26574
Gabriel Krysa <gabrielkrysa@gmail.com>
2020-03-10 17:49:10 +0000

    paralisando

--- crivo.c
+++ crivo.c
@@ -11,28 +11,29 @@ int main()
     {
         vet[i] = i;
     }
-
-#pragma omp parallel for
-    for (int i = 0; i < l; i++)
-    {
-        if (vet[i] % 2 == 0)
+#pragma omp parallel
+{
+    #pragma omp for
+        for (int i = 0; i < l; i++)
         {
-            vet[i] = -1;
+            if (vet[i] % 2 == 0)
+            {
+                vet[i] = -1;
+            }
+            if (vet[i] % 3 == 0 && vet[i] != 3)
+            {
+                vet[i] = -1;
+            }
+            if (vet[i] % 5 == 0 && vet[i] != 5)
+            {
+                vet[i] = -1;
+            }
+            if (vet[i] % 7 == 0 && vet[i] != 7)
+            {
+                vet[i] = -1;
+            }
         }
-        if (vet[i] % 3 == 0 && vet[i] != 3)
-        {
-            vet[i] = -1;
-        }
-        if (vet[i] % 5 == 0 && vet[i] != 5)
-        {
-            vet[i] = -1;
-        }
-        if (vet[i] % 7 == 0 && vet[i] != 7)
-        {
-            vet[i] = -1;
-        }
-    }
-    
+}   
     for (int i = 0; i < l; i++)
     {
         if (vet[i] != -1 && vet[i] > 0)
