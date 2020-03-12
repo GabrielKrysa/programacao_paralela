6d8b94586b2169f36b2e8ded78ffc7a39e2a230a
Gabriel Krysa <gabrielkrysa@gmail.com>
2020-03-12 16:42:14 +0000

    fix bug

--- numero_amigos.c
+++ numero_amigos.c
@@ -1,36 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "omp.h"
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-    const int N = 50000;
-	double parallel[N];
+int main() {
+  const int N = 50000;
+  double parallel[N];
+  int sum = 0;
 
-	#pragma omp parallel
-	{
-		for (int i = 1; i < N; i++)
-		{
-            int sum = 0;
-            for(int j = 1; j <= i;j++)
-            {
-                if(i % j == 0)
-                {
-                    sum += j;    
-                }
-            }
-
-            parallel[i] = (double) sum / i;
-		}
-	}
-
-    for(int k = 1; k < N; k++){
-            for(int l = 1; l < k; l++){
-                if(parallel[k] == parallel[l] && k != l){
-                    printf("%d eh amigo de %d\n",k,l);
-                }
-            }
+#pragma omp parallel
+  {
+#pragma omp for reduction(+ : sum)
+    for (int i = 1; i < N; i++) {
+      sum = 0;
+      for (int j = 1; j <= i; j++) {
+        if (i % j == 0) {
+          sum += j;
         }
-    return 0;
+      }
+      parallel[i] = (double)sum / i;
+    }
+  }
+
+  for (int k = 1; k < N; k++) {
+    for (int l = 1; l < k; l++) {
+      if (parallel[k] == parallel[l] && k != l) {
+        printf("%d eh amigo de %d\n", k, l);
+      }
+    }
+  }
+  return 0;
 }
